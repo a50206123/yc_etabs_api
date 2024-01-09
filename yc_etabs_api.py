cbd16187e7dbfab2710c702c9e166ff21975af54
= <=>
2024-01-09 21:27:59 +0800

    Dev

--- yc_etabs_api.py
+++ yc_etabs_api.py
@@ -10,15 +10,19 @@ class etabs() :
         
         try:  
             #To get the active ETABS object
-            self.etabs = helper.GetObject("CSI.ETABS.API.ETABSObject")
+            etabs = helper.GetObject("CSI.ETABS.API.ETABSObject")
         except (OSError, comtypes.COMError):
             print("No running instance of the program found or failed to attach.")
             sys.exit(-1)
         
         print("Successfully Loaded")
-        self.sap = self.etabs.SapModel
+        sap = etabs.SapModel
         
-        # self.set_units()
+        self.set_units()
+        
+        self.helper = helper
+        self.etabs = etabs
+        self.sap = sap
         
     def set_units(self, units = ['tf', 'm']) :
         if units == ['tf', 'm'] :
@@ -28,9 +32,16 @@ class etabs() :
             pass
         
         self.sap.SetPresentUnits(unit)
+        
+    def create_pt(self, x, y, z) :
+        # Return PtLabel, returnValue
+        return self.sap.PointObj.AddCartesian(x, y, z)
             
 if __name__ == '__main__' :
-    sap = etabs().sap
+    et = etabs()
+    sap = et.sap
     
-    ret = sap.PropMaterial.SetMaterial('CONC', 2)
-    ret = sap.PropMaterial.SetMPIsotropic('CONC', 3600, 0.2, 0.0000055)
+    # ret = sap.PropMaterial.SetMaterial('CONC', 2)
+    # ret = sap.PropMaterial.SetMPIsotropic('CONC', 3600, 0.2, 0.0000055)
+    
+    print(et.create_pt(0,0,10))
