dcdd0c1b418b104e15cfab4d05920aab6b282c2c
YCLin win11 <a50206123@gmail.com>
2024-03-24 07:34:08 +0800

    TEST Analyze

--- analyze.py
+++ analyze.py
@@ -6,7 +6,17 @@ class Analyze() :
         self.sapModel = etabs.sapModel
 
     def run(self) :
-        self.sapModel.analyze.RunAnalysis()
+        print(f'{" Analysis STARTS ":-^30}')
+
+        ret = self.sapModel.analyze.RunAnalysis()
+
+        if ret == 0 :
+            log = "Analysis is finished."
+        else :
+            log = "Analysis is NOT finished"
+        
+        print(f'{" " + log + " ":-^30}')
+        # print(ret)
 
     def set_case_to_run(self, load_cases:list =[]):
         ## load_cases = [] --> All case to run
@@ -17,3 +27,10 @@ class Analyze() :
                     self.SapModel.Analyze.SetRunCaseFlag(lc, False)
             else:
                 self.SapModel.Analyze.SetRunCaseFlag(lc, True) 
+
+if __name__ == '__main__' :
+    from yc_etabs_api.etabs import ETABS
+
+    etabs = ETABS()
+
+    etabs.Analyze.run()
